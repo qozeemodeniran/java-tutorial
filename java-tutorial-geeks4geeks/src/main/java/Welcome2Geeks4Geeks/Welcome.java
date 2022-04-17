c65b0892863b93da00ed9ae265452455e567208b
Qozeem Odeniran <muhdqozeem@gmail.com>
2022-04-17 22:41:40 +0100

    initial commit

---
+++ java-tutorial-geeks4geeks/src/main/java/Welcome2Geeks4Geeks/Welcome.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Welcome2Geeks4Geeks;
+
+/**
+ *
+ * @author qozeemodeniran
+ */
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Geek4Keeks");
+    }
+}
